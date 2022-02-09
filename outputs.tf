73ce8a11aa71b290cac8267fda963b6608a2ab66
Dmitriy <dspv@me.com>
2022-02-09 15:36:00 +0200

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "load_balancer_arn" {
+    value = aws_lb.public.arn
+}
+
+output "load_balancer_fqdn" {
+    value = aws_lb.public.dns_name
+}
+
+output "name" {
+    value = aws_route53_record.main.name
+}
+
+output "fqdn" {
+    value = aws_route53_record.main.fqdn
+}
+
+output "app_port" {
+    value = var.app_port
+}
