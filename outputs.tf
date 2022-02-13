fb507f2294699a7cb056fec85c98be157cf05d07
Dmitriy <dspv@me.com>
2022-02-13 15:13:48 +0200

    Conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-# output "load_balancer_arn" {
-#     value = aws_lb.public.*.arn
-# }
+output "load_balancer_arn" {
+    value = var.public == true ? aws_lb.public[0].arn : null
+}
 
-# output "load_balancer_fqdn" {
-#     value = aws_lb.public.*.dns_name
-# }
+output "load_balancer_fqdn" {
+    value = var.public == true ? aws_lb.public[0].fqdn : null
+}
 
 # output "name" {
-#     value = aws_route53_record.main.name
+#     value = aws_route53_record.main[0].name
 # }
 
 # output "fqdn" {
-#     value = aws_route53_record.main.fqdn
+#     value = aws_route53_record.main[0].fqdn
 # }
 
-# output "app_port" {
-#     value = var.app_port
-# }
+output "app_port" {
+    value = var.app_port
+}
 
 output cloudwatch_log_group_name {
     value = aws_cloudwatch_log_group.ecs_group.name
