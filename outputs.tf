fcc32f50aa1fe984369cbd79926befb4e28dd1ab
Dmitriy <dspv@me.com>
2022-02-13 15:04:38 +0200

    Hide outputs for a while

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "load_balancer_arn" {
-    value = aws_lb.public.*.arn
-}
+# output "load_balancer_arn" {
+#     value = aws_lb.public.*.arn
+# }
 
-output "load_balancer_fqdn" {
-    value = aws_lb.public.*.dns_name
-}
+# output "load_balancer_fqdn" {
+#     value = aws_lb.public.*.dns_name
+# }
 
-output "name" {
-    value = aws_route53_record.main.name
-}
+# output "name" {
+#     value = aws_route53_record.main.name
+# }
 
-output "fqdn" {
-    value = aws_route53_record.main.fqdn
-}
+# output "fqdn" {
+#     value = aws_route53_record.main.fqdn
+# }
 
-output "app_port" {
-    value = var.app_port
-}
+# output "app_port" {
+#     value = var.app_port
+# }
 
 output cloudwatch_log_group_name {
     value = aws_cloudwatch_log_group.ecs_group.name
